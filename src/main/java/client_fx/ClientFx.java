725c7be14302a7093de6e9c48a3e3e38cc4f2b61
NICooper <nicholas.i.cooper@gmail.com>
2023-03-31 18:34:32 -0400

    Setup de base
    creation de javafx jar fonctionne

---
+++ src/main/java/client_fx/ClientFx.java
@@ -0,0 +1,21 @@
+package client_fx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ClientFx extends Application {
+    @Override
+    public void start(Stage stage) {
+        VBox root = new VBox();
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main() {
+        launch();
+    }
+}
