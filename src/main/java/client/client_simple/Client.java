375270e5b149b680a62fcf0a2e5f6684c2722b5e
hyuvb <115302290+hyuvb@users.noreply.github.com>
2023-04-11 20:03:43 -0400

    Écriture du commentaire

--- src/main/java/client/client_simple/Client.java
+++ src/main/java/client/client_simple/Client.java
@@ -10,15 +10,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Le programme qui se connecte au serveur pour lui envoyer des requêtes
+ */
 public class Client {
     static ArrayList<Course> courses;
 
+    /**
+     * Imprime le message de bienvenue et démarre les deux methods getAvailableCourses() et
+     * inscriptionToCourse().
+     * @param args pas utilisé
+     */
     public static void main(String[] args)  {
 
         System.out.println("*** Bienvenue au portail d'inscription de cours de l'UDEM ***");
         getAvailableCourses();
+        inscriptionToCourse();
     }
 
+    /**
+     * Affiche le message au client pour choisir la session et récupère la liste disponible
+     * pour une session donnée, après affiche la liste des cours sous le format donné.
+     *
+     * courses est la liste des cours retournée par le Server.
+     * @throws IOException erreur d'entrée/sortie
+     * @throws ClassNotFoundException erreur quand aucune définition de la classe Course
+     * n'a pu être trouvé
+     */
     public static void getAvailableCourses() {
         System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours:");
         System.out.println("1. Automne\n2. Hiver\n3. Ete");
@@ -58,12 +76,24 @@ public class Client {
             os.close();
             inscriptionToCourse();
 
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Erreur à l'ouverture/écriture du fichier");
+        } catch (ClassNotFoundException ce) {
+            System.out.println("La class lue n'existe pas dans le programme");
         }
     }
 
-
+    /**
+     * Récupère les données nécessaires afin de s'inscrire à un cours.
+     * Donne le choix de consulter une autre session ou de faire l'inscription au client. Si le client
+     * choisit de s'inscrire au cours, il doit donner les informations requises, y compris le code du cours.
+     * Le code du cours doit être présent dans la liste des cours disponibles dans la session en question.
+     * Après le client va envoyer toutes les informations sous la forme d'un Objet RegistrationForm.
+     * @throws IOException erreur d'entrée/sortie
+     * @throws ClassNotFoundException erreur quand aucune définition de la classe RegistrationForm
+     * n'a pu être trouvé
+     */
     public static void inscriptionToCourse() {
         System.out.println("Choix:");
         System.out.println("1. Consulter les cours offerts pour une autre session");
@@ -130,8 +160,9 @@ public class Client {
 
         } catch (IOException ex) {
             ex.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Erreur à l'ouverture/écriture du fichier");
+        } catch (ClassNotFoundException ce) {
+            System.out.println("La class lue n'existe pas dans le programme");
         }
     }
 }
--- src/main/java/server/ServerLauncher.java
+++ src/main/java/server/ServerLauncher.java
@@ -1,8 +1,19 @@
 package server;
 
+/**
+ * Le point de connection qui démarre le serveur.
+ */
 public class ServerLauncher {
+    /**
+     *  Le port auquel le serveur doit se connecter
+     */
     public final static int PORT = 1337;
 
+    /**
+     * Initialise le serveur avec le port spécifié et appelle le method run().
+     * @param args pas utilisé
+     * @throws Exception apparition d'une situation anormale qui conduirait à l'échec du programme
+     */
     public static void main(String[] args) {
         Server server;
         try {
