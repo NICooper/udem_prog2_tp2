9b843b4dae6d6b4001fad3f6f43178cf65e590ac
NICooper <nicholas.i.cooper@gmail.com>
2023-04-17 16:27:44 -0400

    Merge manuel des commentaires de Hongyu

--- src/main/java/views/client_simple/Client.java
+++ src/main/java/views/client_simple/Client.java
@@ -12,10 +12,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Le programme qui se connecte au serveur pour lui envoyer des requêtes
+ */
 public class Client {
     static ClientController controller;
     static List<Course> courses;
 
+    /**
+     * Imprime le message de bienvenue et démarre les deux methods getAvailableCourses() et
+     * inscriptionToCourse().
+     * @param args pas utilisé
+     */
     public static void main(String[] args)  {
         controller = new ClientController(new RemoteCourseList(), new RemoteCourseRegistration());
 
@@ -23,6 +31,12 @@ public class Client {
         getAvailableCourses();
     }
 
+    /**
+     * Affiche le message au client pour choisir la session et récupère la liste disponible
+     * pour une session donnée, après affiche la liste des cours sous le format donné.
+     * courses est la liste des cours retournée par le Server.
+     * n'a pu être trouvé
+     */
     private static void getAvailableCourses() {
         System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours:");
         System.out.println("1. Automne\n2. Hiver\n3. Ete");
@@ -57,7 +71,14 @@ public class Client {
         inscriptionToCourse();
     }
 
-
+    /**
+     * Récupère les données nécessaires afin de s'inscrire à un cours.
+     * Donne le choix de consulter une autre session ou de faire l'inscription au client. Si le client
+     * choisit de s'inscrire au cours, il doit donner les informations requises, y compris le code du cours.
+     * Le code du cours doit être présent dans la liste des cours disponibles dans la session en question.
+     * Après le client va envoyer toutes les informations sous la forme d'un Objet RegistrationForm.
+     * n'a pu être trouvé
+     */
     private static void inscriptionToCourse() {
         System.out.println("Choix:");
         System.out.println("1. Consulter les cours offerts pour une autre session");
